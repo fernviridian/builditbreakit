f73fe606b8f9ac0a2bf4fa66c25da3eb2b2e35e7
George Nicol <piratezpdx@gmail.com>
2014-08-31 11:11:27 -0700

    alter to accept count at command line

--- build/batchmaker.py
+++ build/batchmaker.py
@@ -2,12 +2,14 @@
 # rainy_day_hackers
 # August 30, 2014
 
+import sys
+
 filename='thebatch'
 token='token123'
 name1='emp'
 name2='guest'
 j=3
-count
+count=int(sys.argv[1])
 print("-T 0 -K {1} -E {2} -A {3}".format(j,token,name1,filename))
 print("-T 1 -K {1} -E {2} -A {3}".format(j,token,name2,filename))
 while j < count:
