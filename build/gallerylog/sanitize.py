8693ab6418a030ec2e772e13c2892e41761bdf88
George Nicol <piratezpdx@gmail.com>
2014-09-06 11:18:52 -0700

    modified regex to for loop to evaluate performance

--- build/gallerylog/sanitize.py
+++ build/gallerylog/sanitize.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import re
+
+# --going to have a performance comparison here
+import unicodedata
+import string
+# import re
+
+zero=ord('0')
+nine=ord('9')
+capA=ord('A')
+capZ=ord('Z')
 
 # incoming is the argument from parser like an employee name
 # or a room-id
@@ -9,31 +18,56 @@ import re
 # returns false if any portion of the incoming string is not 0-9
 
 def sanitizeNumber(incoming):
-    if (re.compile("[^0-9]").search(incoming)):
-        return False #return false if we find anything that isnt alphanumeric
-    else:
-        return True #if it is a valid string, return True
+#    if (re.compile("[^0-9]").search(incoming)):
+#        return False #return false if we find anything that isnt alphanumeric
+#    else:
+#        return True #if it is a valid string, return True
+    flag = True
+    for item in incoming:
+        val=ord(item)
+        if val > nine or val < zero:
+            flag = False
+            return flag
+    return flag
+
+
+
+
 
 # ----------------------------
 # returns false if any portion of the incoming string is not
 # an ASCII letter
 
 def sanitizeAlpha(incoming):
-    if (re.compile("[^a-zA-Z]").search(incoming)):
-        return False #return false if we find anything that isnt alphanumeric
-    else:
-        return True #if it is a valid string, return True
-
+#    if (re.compile("[^a-zA-Z]").search(incoming)):
+#        return False #return false if we find anything that isnt alphanumeric
+#    else:
+#        return True #if it is a valid string, return True
+    flag = True
+    incoming=incoming.upper
+    for item in incoming:
+        val=ord(item)
+        if val > capZ or val < capA:
+            flag = False
+            return flag
+    return flag
 # ---------------------------------
 # returns false if any portion of the incoming string is not
 # either an ASCII letter or 0-9
 
 def sanitizeAlphanumeric(incoming):
-    if (re.compile("[^0-9a-zA-Z]").search(incoming)):
-        return False #return false if we find anything that isnt alphanumeric
-    else:
-        return True #if it is a valid string, return True
-
+#    if (re.compile("[^0-9a-zA-Z]").search(incoming)):
+#        return False #return false if we find anything that isnt alphanumeric
+#    else:
+#        return True #if it is a valid string, return True
+    flag = True
+    incoming=incoming.upper
+    for item in incoming:
+        val=ord(item)
+        if val > capZ or val < capA:
+            flag = False
+            return flag
+    return flag
 
 if __name__=='__main__':
     alphalist='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'    # string containing only ascii letters
